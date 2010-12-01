f1dc56037ca0f9950f35e0920f8b946b07e30a4f
Byrne <byrne@majordojo.com>
2010-12-01 12:30:14 -0800

    More testing to make sure the darn thing works.

--- plugins/PurgeActivityLog/lib/PurgeActivityLog/Plugin.pm
+++ plugins/PurgeActivityLog/lib/PurgeActivityLog/Plugin.pm
@@ -25,24 +25,37 @@ sub load_tasks {
 
 sub task_purge {
     my $this = shift;
-    require MT::Util;
     my $mt     = MT->instance;
     my $plugin = MT->component('PurgeActivityLog');
     my $n      = $plugin->get_config_value('purge_n_days','system');
-    my $now    = epoch2ts(undef,time());
-    my $epoch  = time() - (60 * 60 * 24 * $n);
-    my $date   = epoch2ts(undef,$epoch);
     my $total_changed = 0;
-#    print STDERR "Purging activity log entries older than $n days (now = $now; date = $date)\n";
+    $n = 5;
+    my $epoch  = time() - (60 * 60 * 24 * $n);
 
-    my $iter = MT->model('log')->load_iter( { created_on => [ undef, $date ] },
+    require MT::Util;
+    my $blog_iter = MT->model('blog')->load_iter();
+    while (my $blog = $blog_iter->()) {
+        my $now    = epoch2ts($blog,time());
+        my $date   = epoch2ts($blog,$epoch);
+        my $iter = MT->model('log')->load_iter( { created_on => [ undef, $date ], blog_id => $blog->id },
+                                                { range => { created_on => 1 } } );
+        print "Purging activity log for blog: " . $blog->name . " older than $n days.\n";
+        while ( my $log = $iter->() ) {
+            $log->remove();
+            $total_changed++;
+        }
+    }
+    print "Purging system activity log...\n";
+    my $now    = epoch2ts(undef,time());
+    my $date   = epoch2ts(undef,$epoch);
+    print "Purging activity log entries older than $n days.\n";
+    my $iter = MT->model('log')->load_iter( { created_on => [ undef, $date ], blog_id => 0 },
                                             { range => { created_on => 1 } } );
-
     while ( my $log = $iter->() ) {
-#        print STDERR "Removing log entry with created on date of: ".$log->created_on." (less than ".$date."?)\n";
         $log->remove();
         $total_changed++;
     }
+    print "Deleted $total_changed log entries.\n";
     $total_changed > 0 ? 1 : 0;
 }
 
